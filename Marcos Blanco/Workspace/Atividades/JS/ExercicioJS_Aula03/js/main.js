475fb200f75d233703f09f8f00f740e4159ed4d6
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-05-09 21:43:09 -0300

    Aula 03 JS

---
+++ Marcos Blanco/Workspace/Atividades/JS/ExercicioJS_Aula03/js/main.js
@@ -0,0 +1,25 @@
+let fruta1 = " Banana";
+const fruta2 = "Laranja";
+const fruta3 = "Melancia";
+const fruta4 = "Abacaxi";
+const fruta5 = "Maça";
+
+fruta1 = fruta1.trim();
+
+console.log(fruta1.length);
+console.log(fruta2.length);
+console.log(fruta3.length);
+console.log(fruta4.length);
+console.log(fruta5.length);
+
+console.log(`Quantidade letras ${fruta1.length}, ${ fruta2.length}, ${fruta3.length}, ${fruta4.length} e ${fruta5.length}`)
+
+console.log(fruta1.substring(0, 3));
+console.log(fruta2.substring(0, 3));
+console.log(fruta3.substring(0, 3));
+console.log(fruta4.substring(0, 3));
+console.log(fruta5.substring(0, 3));
+
+fruta1 = fruta1.toLowerCase();
+console.log(fruta1);
+
