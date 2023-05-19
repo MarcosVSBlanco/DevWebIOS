5c4d72d613b9a100054048f2df2f1ec3c20db5e7
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-05-18 21:26:49 -0300

    Exercicio 05 JS

--- Marcos Blanco/Workspace/Atividades/JS/ExercicioJS_Aula05/js/main.js
+++ Marcos Blanco/Workspace/Atividades/JS/ExercicioJS_Aula05/js/main.js
@@ -1,13 +1,18 @@
+//CONTAGEM 1-50 while
 let contador = 1;
 while (contador <= 50) {
 console.log(`Valor = ${contador}`);
 contador++;
 }
 
-let i = 0,
-    text = '';
+//CONTAGEM REGRESSIVA 10-0 do-while
+let i = 10,
+text = '';
 do {
     text += `O número é ${i}\n`;
-    i++;
-} while (i < 10);
+i--;
+} while (i > -1);
 console.log(text);
+
+//FOR CONTAGEM PARES 1-100 for
+for (let i = 2; i < 101; i++) console.log(`Laço For Pares: ${i++}`);
