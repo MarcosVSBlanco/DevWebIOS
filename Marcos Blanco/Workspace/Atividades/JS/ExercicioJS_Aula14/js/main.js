386f0f9cc87e833027257aa0cf3530d0d6464975
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-06-01 21:21:19 -0300

    Aula 14 JS

---
+++ Marcos Blanco/Workspace/Atividades/JS/ExercicioJS_Aula14/js/main.js
@@ -0,0 +1,35 @@
+const myForm = document.querySelector('#my-form');
+const item = document.querySelector('#item');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const dataNasc = document.querySelector('#date')
+
+const msg = document.querySelector('.msg');
+const msg_email = document.querySelector('.msg_email');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput.value === '' || dataNasc.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Por favor, preencha os dados.';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        
+        const li = document.createElement('li');
+        li.appendChild(
+            document.createTextNode(
+                `${item.value} : ${nameInput.value} : ${emailInput.value}  `
+            )
+        );
+        userList.appendChild(li);
+        // Limpa o formulário
+        nameInput.value = '';
+        emailInput.value = '';
+        horario.getElementsByTagName('option')[0].selected = 'selected';
+        nameInput.focus(); //Coloca o foco no elmento
+    }
+}
