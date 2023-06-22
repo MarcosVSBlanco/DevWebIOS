16937532c0ccea2c4f1878f2332afeb4eafdb6cc
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-06-21 21:51:50 -0300

    aula 04 React

---
+++ Marcos Blanco/Workspace/React/aula04-react/src/App.js
@@ -0,0 +1,12 @@
+import Exemplo from './components/Exemplo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App;
